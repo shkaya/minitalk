627df05e981fa940a0fcedac91e0affe51999793
shkaya <shuntanumber13@gmail.com>
2024-06-25 22:45:14 +0900

    signal() -> sigaction()

--- client.c
+++ client.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
 
 /*
 	#define SIGUSR1 31 -> ONの時
@@ -22,7 +23,7 @@ void	send_char(pid_t pid, char c)
             // printf("%d: send SIGUSR2\n", i);
         	kill(pid, SIGUSR2); //OFFの時
         }
-    	usleep(100); 
+    	usleep(130); 
         // 次のビットを送る前に、(1 / 1000)秒間、一時停止。 
     }
 }
@@ -31,6 +32,37 @@ void	fd_putstr(char *str, int fd)
 {
     while (*str)
     	write (fd, str++, 1);
+}
+
+void	putnbr(int n)
+{
+    char c;
+
+    if (n > 9)
+    {
+        putnbr(n / 10);
+    }
+	c = '0' + (n % 10);
+    write(1, &c, 1);
+}
+
+void	signal_handle(int signum, siginfo_t *info, void *dummy)
+{
+    static int bit_count = 0;
+    static int n = 0;
+
+	if (signum == SIGUSR1)
+    	n |= 1 << bit_count;
+    bit_count++;
+    if (bit_count == 32)
+    {
+		fd_putstr("Output:", 1);
+		putnbr(n);
+        fd_putstr(" bytes\n", 1);
+		n = 0;
+        bit_count = 0;
+        return;
+    }
 }
 
 int main(int argc, char *argv[])
@@ -43,9 +75,44 @@ int main(int argc, char *argv[])
         exit(EXIT_FAILURE);
     }
 	pid = atoi(argv[1]);
+
+	/*--------- sigactionを使うための設定 -----------*/
+    /*各設定を入れるための構造体を宣言*/
+    struct sigaction	act1; 
+    struct sigaction	act2;
+
+	/*初期化*/
+	memset(&act1, 0, sizeof(sigaction));
+    memset(&act2, 0, sizeof(sigaction));
+    /*ハンドラの設定*/
+    act1.sa_sigaction = signal_handle;
+	act2.sa_sigaction = signal_handle;
+    /*ブロックするシグナルの設定*/
+    sigaddset(&(act1.sa_mask), SIGUSR2);
+    sigaddset(&(act2.sa_mask), SIGUSR1);
+    /*使ってもらうハンドラをsigactionに指定*/
+	act1.sa_flags = SA_SIGINFO;
+    act2.sa_flags = SA_SIGINFO;
+	/*シグナルハンドラ関数*/
+	sigaction(SIGUSR1, &act1, (struct sigaction *)0);
+    sigaction(SIGUSR2, &act2, (struct sigaction *)0);
+
+	/*------------- サーバーへの送信 -------------------------*/	
+    /*1文字ずつに分割*/
 	while (*(argv[2]) != '\0')
 		send_char(pid, *(argv[2]++));
+    /*null文字も送信*/
 	if (*(argv[2]) == '\0')
     	send_char(pid, *(argv[2]));
+
+
+	/*--------------- サーバーからの送信 -----------------------*/
+
+    /*サーバー側からの送信を待つ*/
+	while (1)
+    {
+        pause();
+    }
+
 	return (0);
 }
